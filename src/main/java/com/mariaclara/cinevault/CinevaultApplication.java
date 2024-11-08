04296c0a60126ea78b08285c70b04f8d6e6eeb73
Maria Clara <bmariaclara831@gmail.com>
2024-11-08 15:51:17 -0300

    Primeiro commit

---
+++ src/main/java/com/mariaclara/cinevault/CinevaultApplication.java
@@ -0,0 +1,14 @@
+package com.mariaclara.cinevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class CinevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinevaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariaclara/cinevault/CinevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariaclara.cinevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
